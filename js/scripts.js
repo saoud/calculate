27331fbb5e30a251f37d5d2822e3e88dcde4b169
Collin Chapman <cchap14@gmail.com>
2021-01-25 20:18:06 -0800

    separate business and user interface logic
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
+//Business Logic
 
-function add(number1, number2) {
-    return number1 + number2
-}
+const add = function(number1, number2) {
+    return number1 + number2;
+  };
+const subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+  const multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  
+  const divide = function(number1, number2) {
+    return number1 / number2;
+  };
 
+
+//User Interface Logic
+
+$(document).ready(function() {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
+});
