5286d55ebf840efc2df611db8325a3d0383c4d97
Collin Chapman <cchap14@gmail.com>
2021-01-25 21:04:13 -0800

    add user interface logic for subtract, multiply and divide forms
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,35 @@ $(document).ready(function() {
         $("#output").text(result);
     });
   });
+
+  $(document).ready(function() {
+    $("form#subtract").submit(function(event) {
+        event.preventDefault();
+        const number1 = parseInt($("#subtract1").val());
+        const number2 = parseInt($("#subtract2").val());
+        const result = subtract(number1, number2);
+        $("#output").text(result);
+    });
+  });
+
+
+  $(document).ready(function() {
+    $("form#multiply").submit(function(event) {
+        event.preventDefault();
+        const number1 = parseInt($("#multiply1").val());
+        const number2 = parseInt($("#multiply2").val());
+        const result = multiply(number1, number2);
+        $("#output").text(result);
+    });
+  });
+
+
+  $(document).ready(function() {
+    $("form#divide").submit(function(event) {
+        event.preventDefault();
+        const number1 = parseInt($("#divide1").val());
+        const number2 = parseInt($("#divide2").val());
+        const result = divide(number1, number2);
+        $("#output").text(result);
+    });
+  });
