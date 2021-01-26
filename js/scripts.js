6283d198a631c25934b3e64fc256abbf0676de63
Collin Chapman <cchap14@gmail.com>
2021-01-25 19:53:51 -0800

    change  business logic from multiplication to addition
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
-function multiply(number1, number2) {
-    return number1 * number2
+function add(number1, number2) {
+    return number1 + number2
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = multiply(number1, number2);
+const result = add(number1, number2);
 alert(result);
